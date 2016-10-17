5bc7928e39e27b520d4a8a80ca532454059fb942
AWinnerIsYou <seijiyawata@gmail.com>
2016-10-17 19:51:35 -0400

    Finish basic functions (except random)

--- songnode.c
+++ songnode.c
@@ -16,8 +16,8 @@ void print_list(song_node *list) {
 song_node * insert_front(song_node *list, char *name, char *artist) {
   song_node *new = (song_node *)malloc(sizeof(song_node));
   new->next = list;
-  new->name = name;
-  new->artist = artist;
+  strcpy(new->name, name);
+  strcpy(new->artist, artist);
   return new;
 }
 
@@ -30,14 +30,15 @@ song_node * insert_order(song_node *list, char *name, char *artist) {
     return insert_front(list,name,artist);
 
   // middle
-  song_node ret = list;
-  while (list->next != 0) {
+  song_node *ret = list;
+
+  while (list->next) {
     int artistComp = strcmp(artist, (list->next)->artist);
     int nameComp = strcmp(name, (list->next)->name);
     
     if (artistComp < 0 || (artistComp == 0 && nameComp < 0)) { 
       list->next = insert_front(list->next,name,artist);
-      return list;
+      return ret;
     }
     
     else {
@@ -48,16 +49,15 @@ song_node * insert_order(song_node *list, char *name, char *artist) {
   // end
   song_node *last = (song_node *)malloc(sizeof(song_node));
   list->next = last;
-  last->name = name;
-  last->artist = artist;  
+  strcpy(last->name, name);
+  strcpy(last->artist, artist);  
   return ret;
   
 }
 
-
-song_node * find_song(song_node *list, char *name) {
+song_node * find_song(song_node *list, char *name, char *artist) {
   while (list) {
-    if (strcmp(name, list->name) == 0)
+    if (strcmp(name, list->name) == 0 && strcmp(artist, list->artist) == 0)
       return list;
     else
       list=list->next;
@@ -76,11 +76,27 @@ song_node * find_artist(song_node *list, char *artist) {
 }
 
 song_node * random_song(song_node *list) {
-  
+  return 0;
 }
 
-song_node * remove_song(song_node *list, song_node *remove) {
+song_node * remove_song(song_node *list, char *name, char *artist) {
   
+  // @ beginning
+  if (strcmp(name, list->name) == 0 && strcmp(artist, list->artist) == 0)
+    return list->next;
+  
+  song_node *ret = list; 
+  while (list->next) {
+    if (strcmp(name, (list->next)->name) == 0 && 
+	strcmp(artist, (list->next)->artist) == 0) {
+      list->next = (list->next)->next;
+      return ret;
+    }
+    else
+      list=list->next;
+  }
+  return 0;
+
 }
 
 song_node * free_list(song_node *list) {
@@ -91,4 +107,12 @@ song_node * free_list(song_node *list) {
     temp = list;    
   }
   return list;
+}
+
+int main() {
+  song_node *p_list = 0;
+  p_list = insert_front(p_list, "alive", "pearl jam");
+  p_list = insert_order(p_list,"time", "pink floyd");
+  print_list(p_list);
+  return 0;
 }
--- songnode.h
+++ songnode.h
@@ -8,8 +8,8 @@ typedef struct song_node {
 void print_list(song_node *list);
 song_node * insert_front(song_node *list, char *name, char *artist);
 song_node * insert_order(song_node *list, char *name, char *artist);
-song_node * find_song(song_node *list, char *name);
+song_node * find_song(song_node *list, char *name, char *artist);
 song_node * find_artist(song_node *list, char *artist);
 song_node * random_song(song_node *list);
-song_node * remove_song(song_node *list, song_node *remove);
+song_node * remove_song(song_node *list, char *name, char *artist);
 song_node * free_list(song_node *list);
